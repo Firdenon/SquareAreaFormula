c2d0ff4da96c06cd87bb3c4152d615c071f7e585
Ricky Adi Kuncoro <riq.adi@gmail.com>
2018-08-08 17:58:23 +0700

    asd

--- squareArea/Classes/SquareAreaFormula.swift
+++ squareArea/Classes/SquareAreaFormula.swift
@@ -5,11 +5,12 @@ public class SquareAreaFormula {
     var length: Int = 0
     var width: Int = 0
     
-    init(lenght:Int,width:Int) {
-        
+    public init(length:Int,width:Int) {
+        self.length = length
+        self.width = width
     }
     
-    func areaCalc() -> Int {
+    public func areaCalc() -> Int {
         let area = length*width
         return area
     }
